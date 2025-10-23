5f3afa2c8f874650e31304985f182494cd09345e
Felix-Young-Git <91382176+Felix-Young-Git@users.noreply.github.com>
2025-10-23 15:34:29 -0400

    Magnetic area and stat_mod area
    Renamed the tank_speed_mod_area to tank_stat_mod_area as it can take a stat boost of any type and apply it to the tank. Added the magnetic field

---
+++ game/map_elements/magnetic_area.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+@export var magnetic_strength : int
+#If checked, the bullets entering will experience a clockwise rotation, if not checked, a counter-clockwise rotation
+@export var clockwise_rotation : bool
+var bullets_in_field: Array = []
+
+
+
+#If the bullet is in the field, add a velocity perpendicular to its movtion to make the bullet curve
+func _physics_process(_delta):
+	for bullet in bullets_in_field:
+		var v = bullet.linear_velocity
+		var perpendicular = Vector2(-v.y, v.x)
+
+		if clockwise_rotation:
+			v += perpendicular * magnetic_strength * _delta
+		else:
+			v += perpendicular * magnetic_strength * _delta * -1
+
+		bullet.linear_velocity = v
+
+
+#adds the bullet to the array when it enters the field
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if (body is BasicBullet):
+		if body not in bullets_in_field:
+			bullets_in_field.append(body)
+
+
+#removes the bullet from the array when it exits the field
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if (body is BasicBullet):
+		bullets_in_field.erase(body)
