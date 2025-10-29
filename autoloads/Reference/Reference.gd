ca083fb07a10ee02c8ed2c9af431c0797d768c38
Quintin Levy <159190598+quintinlevy@users.noreply.github.com>
2025-10-28 20:51:33 -0400

    remotebullet + mines implemented

--- autoloads/Reference/Reference.gd
+++ autoloads/Reference/Reference.gd
@@ -44,12 +44,14 @@ class_name _Reference
 @export var tank_scene : PackedScene
 @export var basic_bullet : PackedScene
 @export var multi_bullet : PackedScene
+@export var land_mine : PackedScene
 @export var item_scene : PackedScene
 
 @export_group("Tank Loadouts")
 @export var empty_loadout : PackedScene
 @export var basic_loadout : PackedScene
 @export var multi_loadout : PackedScene
+@export var landmine_loadout : PackedScene
 
 @export_category("Other")
 @export_group("Tank Starting Stats")
