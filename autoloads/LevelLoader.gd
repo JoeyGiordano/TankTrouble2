2f4fd7f0e8f8d012cbf66e5fc604ece7635748bc
JoeyGiordano <74273004+JoeyGiordano@users.noreply.github.com>
2025-12-03 20:18:41 -0500

    Multibullet graphic

--- autoloads/LevelLoader.gd
+++ autoloads/LevelLoader.gd
@@ -23,7 +23,7 @@ func _switch_level(new_level : PackedScene) :
 	Utility.replace_scene_in_holder(Global.LevelHolder, new_level)
 
 func _determine_next_level() -> String :
-	return "test_level_gen_level"
+	return "test_level_3"
 
 func destroy_all_entities() :
 	var x = Global.Entities.get_children()
