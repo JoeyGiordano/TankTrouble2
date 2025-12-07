13fd34e482b61584deb03c5c94a2c3a1915194b7
JoeyGiordano <74273004+JoeyGiordano@users.noreply.github.com>
2025-12-07 16:02:29 -0500

    QUick Fix

--- shell_scenes/startup_cutscene.gd
+++ shell_scenes/startup_cutscene.gd
@@ -3,7 +3,7 @@ extends Sprite2D
 func _ready() -> void:
 	get_parent().get_child(0).play("startup_anim")
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("DEBUG_SKIP") :
 		next_scene()
 
