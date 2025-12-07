8501c31910f9e11f5fda7308215ad0f0e3ed23d3
JoeyGiordano <74273004+JoeyGiordano@users.noreply.github.com>
2025-12-07 15:40:52 -0500

    Cat anim

--- shell_scenes/startup_cutscene.gd
+++ shell_scenes/startup_cutscene.gd
@@ -1,25 +1,7 @@
 extends Sprite2D
 
 func _ready() -> void:
-	await get_tree().create_timer(2).timeout
-	frame = 1
-	await get_tree().create_timer(0.07).timeout
-	frame = 2
-	await get_tree().create_timer(0.1).timeout
-	frame = 3
-	await get_tree().create_timer(0.15).timeout
-	frame = 4
-	AudioManager.play(Ref.pipe_fall)
-	await get_tree().create_timer(0.2).timeout
-	frame = 5
-	await get_tree().create_timer(3).timeout
-	frame = 6
-	await get_tree().create_timer(2).timeout
-	frame = 7
-	await get_tree().create_timer(1.3).timeout
-	frame = 8
-	await get_tree().create_timer(1.7).timeout
-	next_scene()
+	get_parent().get_child(0).play("startup_anim")
 	
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("DEBUG_SKIP") :
