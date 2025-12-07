30ba4609cba16eebeda745068e62e13c3236368b
JoeyGiordano <74273004+JoeyGiordano@users.noreply.github.com>
2025-12-07 16:27:54 -0500

    Loading and rand backgrounds

--- autoloads/LevelLoader.gd
+++ autoloads/LevelLoader.gd
@@ -23,7 +23,7 @@ func _switch_level(new_level : PackedScene) :
 	Utility.replace_scene_in_holder(Global.LevelHolder, new_level)
 
 func _determine_next_level() -> String :
-	return "custom_level_2"
+	return "rng_level_1"
 	var r = randf() * 100
 	if r < 10 :
 		return "custom_level_1"
@@ -33,8 +33,6 @@ func _determine_next_level() -> String :
 		return "test_level_3"
 	elif r < 40 :
 		return "rng_level_1"
-	elif r < 50 :
-		return "rng_level_2"
 	else :
 		return "rng_level_3"
 
