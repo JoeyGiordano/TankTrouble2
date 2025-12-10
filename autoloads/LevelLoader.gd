38f3da0dd183e1d9b9b0df30b2410e58ee38b6a7
JoeyGiordano <74273004+JoeyGiordano@users.noreply.github.com>
2025-12-10 17:01:32 -0500

    Fixed saturation

--- autoloads/LevelLoader.gd
+++ autoloads/LevelLoader.gd
@@ -23,6 +23,7 @@ func _switch_level(new_level : PackedScene) :
 	Utility.replace_scene_in_holder(Global.LevelHolder, new_level)
 
 func _determine_next_level() -> String :
+	return "custom_level_1"
 	var r = randf() * 100
 	if r < 5 :
 		return "custom_level_1"
