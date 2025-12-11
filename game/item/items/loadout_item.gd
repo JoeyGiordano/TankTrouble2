2db8b149a8285b461331881b209c647c4d6a869e
JoeyGiordano <74273004+JoeyGiordano@users.noreply.github.com>
2025-12-11 03:25:01 -0500

    Final Touches

--- game/item/items/loadout_item.gd
+++ game/item/items/loadout_item.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+@export var loadout : String
+
+func _ready() -> void:
+	if loadout != "" :
+		var r : ItemResource = $Item.item_res.duplicate()
+		r.loadout_name = loadout
+		$Item.item_res = r
+			
+
 func _process(_delta: float) -> void:
 	if get_child_count() != 4 :
 		queue_free()
